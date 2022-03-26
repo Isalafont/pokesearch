f66f2cde6621751d6dc9e1348204d49b6c1b8eb3
Isabelle Lafont <isalafont@gmail.com>
2022-03-26 15:36:45 +0100

    Add Spinner component and try to implement it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import getPokemon from '../api/getPokemon'
 import SearchBar from './SearchBar'
 import PokemonCard from './PokemonCard'
-import { toHaveFocus } from '@testing-library/jest-dom/dist/matchers'
+import Spinner from './Spinner'
+
 
 class App extends React.Component {
   state = {
-    error: null,
     isLoaded: false,
     pokemon: [],
+    errorMessage: '',
   }
 
   onSearchSubmit = async (term) => {
@@ -18,57 +19,44 @@ class App extends React.Component {
     this.setState({
       // isLoaded: true,
       pokemon: {
-        name: response.data.forms[0].name,
-        numberDex: response.data.id,
+        name: response.data.name,
+        id: response.data.id,
         height: response.data.height,
         weight: response.data.weight,
-        spritePokemon: response.data.sprites.front_default,
         picture: response.data.sprites.other.dream_world.front_default,
-        typePokemon: response.data.types,
+        smPicture: response.data.sprites.front_default,
+        baseXp: response.data.base_experience,
+        type: response.data.types[0].type.name,
+        species: response.data.species.name,
         movesPokemon: response.data.moves,
-        abilityPokemon: response.data.abilities[1].name,
+        ability: response.data.abilities[1].name,
         stats: response.data.stats,
       },
-    })
-    // (error) => {
-    //   this.setState({
-    //     isLoaded: true,
-    //     error,
-    //   })
-    // }
+    }),
+    (error) => this.setState({ isLoaded: true, errorMessage: err.message })
+    }
+  }
+  
+  renderContent(){
+    if (this.state.errorMessage && !this.state.pokemon) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.pokemon) {
+      return <div><PokemonCard poke={this.state.pokemon} />
+        </div>
+    }
+    return <Spinner message="Catching your Pokemon!" />
   }
 
-  render() {
-    // const { error, isLoaded, pokemon } = this.state
-    //     if (error) {
-    //       return <div>Error : {error.message}</div>
-    //     } else if (!isLoaded) {
-    //       return <div>Catching your pokemon...</div>
-    //     } else {
+  render(){
     return (
       <div className="w-full h-full">
         <div className="w-full h-full bg-gray-50">
           <SearchBar onSubmit={this.onSearchSubmit} />
         </div>
         <div>
-          <PokemonCard poke={this.state.pokemon} />
+          {this.renderContent()}
         </div>
-        {/* <div className="w-full h-full flex flex-col justify-center py-6">
-          <img src={this.state.pokemon.picture} />
-          <h2>Name: {this.state.pokemon.namePokemon}</h2>
-          <p>ID: {this.state.pokemon.numberDex}</p>
-          <p>Height: {this.state.pokemon.height}</p>
-          <p>Weight: {this.state.pokemon.weight} </p>
-          <p>
-            {/* <div>
-              {this.state.pokemon.stats.map((data, i) => (
-                <li key={`${data.stat.name}-${i}`}>
-                  {data.stat.name}: <span>{data.base_stat}</span>
-                </li>
-              ))}
-            </div>
-          </p>
-        </div> */}
       </div>
     )
   }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,28 +3,31 @@ import React from 'react'
 const PokemonCard = (props) => {
   console.log(props.poke)
   const poke = props.poke
-  // const pokeName = props.poke.name
+  const stats = props.poke.stats
+  console.log(stats)
 
   return (
     <div className="m-12 flex justify-center">
-      <div className="p-5 max-w-2xl flex flex-col bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg" src={poke.picture} alt="pokemon" />
+      <div className="p-5 w-96 max-w-2xl flex flex-col bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <img className="rounded-t-lg" src={poke.picture} alt={poke.name} />
         <div className=" m-3 p-8 flex flex-col">
-          <h5 className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-center text-2xl capitalize font-bold tracking-tight text-gray-900 dark:text-white">
             {poke.name}
           </h5>
-          <p className="mb-3 font-bold tracking-tight dark:text-gray-400 dark:text-white">
-            Height :{poke.height}
+          <p className="mb-3 pt-2 capitalize font-light tracking-tight dark:text-gray-400 dark:text-white">
+            Type : {poke.type}
           </p>
-          <p className="mb-3 font-bold tracking-tight dark:text-gray-400 dark:text-white">
-            Weight :{poke.weight}
+          <p className="mb-3 capitalize font-light tracking-tight dark:text-gray-400 dark:text-white">
+            Specie : {poke.species}
           </p>
-          <p className="mb-3">
-            <img
-              className="rounded-t-lg"
-              src={poke.spritePokemon}
-              alt="little pokemon"
-            />
+          <p className="mb-3 font-light tracking-tight dark:text-gray-400 dark:text-white">
+            Experience : {poke.baseXp}
+          </p>
+          <p className="mb-3 font-light tracking-tight dark:text-gray-400 dark:text-white">
+            Height : {poke.height}
+          </p>
+          <p className="mb-3 font-light tracking-tight dark:text-gray-400 dark:text-white">
+            Weight : {poke.weight}
           </p>
         </div>
       </div>
